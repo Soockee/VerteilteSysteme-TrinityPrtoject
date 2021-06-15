0fd39c99b6412afa55390101161defb96559b532
Björn Franke <bjoern.franke@mni.thm.de>
2021-06-15 21:58:34 +0200

    Fix condition-serialization

--- headquarter_service/src/main/kotlin/com/microservices/headquarterservice/controller/ConditionController.kt
+++ headquarter_service/src/main/kotlin/com/microservices/headquarterservice/controller/ConditionController.kt
@@ -2,20 +2,13 @@ package com.microservices.headquarterservice.controller
 
 import com.microservices.headquarterservice.model.Condition
 import com.microservices.headquarterservice.model.ConditionResponse
-
 import com.microservices.headquarterservice.service.ConditionService
-import org.springframework.amqp.core.Queue
-import org.springframework.amqp.rabbit.core.RabbitTemplate
-import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
-import java.util.UUID
 
 @RestController("ConditionController")
 class ConditionController(
@@ -26,8 +19,8 @@ class ConditionController(
     fun create(
         @RequestBody condition: Condition
     ): Mono<Condition> {
-        var conditionResult: Mono<Condition> = conditionService.create(condition)
-        var conditionResponse: ConditionResponse = ConditionResponse(condition.conditions_id, condition.supplier_id, condition.price, condition.negotiation_timestamp)
+        val conditionResult: Mono<Condition> = conditionService.create(condition)
+        val conditionResponse = ConditionResponse(condition.conditions_id, condition.supplier_id, condition.price, condition.negotiation_timestamp)
         conditionService.send(conditionResponse)
         return conditionResult
     }
--- headquarter_service/src/main/kotlin/com/microservices/headquarterservice/model/ConditionResponse.kt
+++ headquarter_service/src/main/kotlin/com/microservices/headquarterservice/model/ConditionResponse.kt
@@ -1,15 +1,66 @@
 package com.microservices.headquarterservice.model
 
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import org.springframework.data.annotation.Id
-import java.util.UUID
 import java.math.BigDecimal
 import java.sql.Timestamp
-import kotlinx.serialization.Serializable
+import java.text.DecimalFormat
+import java.util.*
 
 @Serializable
 data class ConditionResponse(
-    @Id var conditions_id: UUID?,
-    var supplier_id: UUID?,
-    var price: BigDecimal,
-    var negotioation_timestamp: Timestamp?,
+
+        @Id
+        @Serializable(with = UUIDSerializer::class)
+        var conditions_id: UUID?,
+        @Serializable(with = UUIDSerializer::class)
+        var supplier_id: UUID?,
+        @Serializable(with = BigDecimalSerializer::class)
+        var price: BigDecimal,
+        @Serializable(with = TimestampSerializer::class)
+        var negotioation_timestamp: Timestamp?,
 )
+
+object UUIDSerializer : KSerializer<UUID> {
+    override fun deserialize(decoder: Decoder): UUID {
+        return UUID.fromString(decoder.decodeString())
+    }
+
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("UUID", PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: UUID) {
+        return encoder.encodeString(value.toString())
+    }
+}
+
+object BigDecimalSerializer : KSerializer<BigDecimal> {
+    override fun deserialize(decoder: Decoder): BigDecimal {
+        val df = DecimalFormat()
+        df.isParseBigDecimal = true
+        return df.parse(decoder.decodeString()) as BigDecimal
+    }
+
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("BigDecimal", PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: BigDecimal) {
+        return encoder.encodeString(value.toString())
+    }
+}
+
+object TimestampSerializer : KSerializer<Timestamp> {
+    override fun deserialize(decoder: Decoder): Timestamp {
+        return Timestamp.valueOf(decoder.decodeString())
+    }
+
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("Timestamp", PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: Timestamp) {
+        return encoder.encodeString(value.toString())
+    }
+}
