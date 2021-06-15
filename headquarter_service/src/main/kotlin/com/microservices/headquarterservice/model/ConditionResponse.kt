edbdd41f8c7cf1e566c974c0ea0f7d20df2e30ec
Soockee <gamingsockee@gmail.com>
2021-06-15 17:53:59 +0200

    not working serialization

--- headquarter_service/src/main/kotlin/com/microservices/headquarterservice/model/ConditionResponse.kt
+++ headquarter_service/src/main/kotlin/com/microservices/headquarterservice/model/ConditionResponse.kt
@@ -4,7 +4,9 @@ import org.springframework.data.annotation.Id
 import java.util.UUID
 import java.math.BigDecimal
 import java.sql.Timestamp
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class ConditionResponse(
     @Id var conditions_id: UUID?,
     var supplier_id: UUID?,
