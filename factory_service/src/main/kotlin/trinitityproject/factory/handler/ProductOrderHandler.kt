15120b944abb617cef6ffc318ef3b115983f74c2
fdrs33 <fabian.drees@mni.thm.de>
2021-06-25 00:41:37 +0200

    Added TimeService and ReportService (WIP)

--- factory_service/src/main/kotlin/trinitityproject/factory/handler/ProductOrderHandler.kt
+++ factory_service/src/main/kotlin/trinitityproject/factory/handler/ProductOrderHandler.kt
@@ -129,84 +129,8 @@ class ProductOrderHandler(
         return ok().buildAndAwait()
     }
 
-    /**
-     * Creates the partOrders for a given productOrder
-     *
-     * @param productOrderId Id of the ProductOrder for which the PartOrders are to be created
-     */
-    suspend fun createPartOrders(productOrderId: UUID) {
-        val productOrderFlow = repository.findById(productOrderId).asFlow();
 
-        if (productOrderFlow.count() < 1) {
-            return;
-        }
 
-        var productOrder = productOrderFlow.toList().first();
-        val products = productOrder.products.toMutableList();
-
-        var neededParts : MutableMap<UUID, Number> = HashMap()
-
-        products.forEach { product ->
-            product.parts.forEach { part ->
-                if(neededParts.containsKey(part.partId)) {
-                    neededParts.set(part.partId, neededParts.getValue(part.partId).toInt() + part.count.toInt());
-                } else {
-                    neededParts.set(part.partId, part.count);
-                }
-            };
-        };
-
-        productOrder.partOrders = this.toPartOrders(neededParts);
-
-        // TODO(Fabian): Submit PartOrders to corresponding Supplier
-    }
-
-    // TODO(Fabian): get ID and Count of every Part und check, where to order it
-    fun toPartOrders(neededParts: MutableMap<UUID, Number>): List<PartOrder> {
-        var partOrdersMap: MutableMap<UUID, PartOrder> = HashMap();
-        var partOrders: MutableList<PartOrder> = ArrayList();
-
-        neededParts.keys.forEach { partId ->
-            val supplierId: UUID = this.getSuitableSupplier(partId);
-            if(partOrdersMap.containsKey(supplierId)) {
-                var partOrder: PartOrder = partOrdersMap.get(supplierId)!!;
-                //partOrder.positions
-            } else {
-                var positions: MutableList<Position> = ArrayList();
-
-                // TODO(Fabian): Get partId from Supplier
-                // TODO(Fabian): Get conditionId
-                // positions.add(Position(partId, neededParts.get(partId))
-                // var partOrder = PartOrder(UUID.randomUUID(), Status.OPEN, supplierId, );
-
-            }
-        }
-
-        return partOrders;
-    }
-
-    // TODO(Fabian): Check, where to order the part
-    fun getSuitableSupplier(partId: UUID): UUID {
-        return UUID.randomUUID();
-    }
-
-    // TODO(Fabian): PartOrder Updates fertigstellen
-    /**
-     * Updates the status of a partOrder
-     *
-     * @param productOrderId Id of the ProductOrder which contains the partOrder to be updated
-     * @param id Id of the PartOrder to be updated
-     * @param status status to be set
-     */
-    suspend fun updatePartOderStatus(productOrderId: UUID, partOrderId: UUID, status: Status) {
-        val productOrderFlow = repository.findById(productOrderId).asFlow();
-
-        if (productOrderFlow.count() < 1) {
-            return;
-        }
-
-        var productOrder = productOrderFlow.toList().first();
-    }
 
     /**
      * Updates the status of a product in a productOrder
@@ -262,28 +186,4 @@ class ProductOrderHandler(
 //    - update partOrder list -> Es muss moeglich sein neue partOrders hinzuzufuegen
 //    - update Status -> ProductOrder,PartOrder, Product
 //    - evtl. delete routen
-
-    /**
-     * Angeben, welche Parts bei welchem Lieferanten geordert werden sollen?
-     *
-     *
-     * @param productOrderId Id of the product order for which parts are to be ordered
-     * @param supplierId Id of the supplier with whom the order is to be placed
-     * @param positions List of the positions to be ordered
-     */
-    suspend fun addPartOrder(productOrderId: UUID, supplierId: UUID, positions: List<Position>) {
-        val partOrder = PartOrder(UUID.randomUUID(), Status.OPEN, supplierId, positions);
-
-        val productOrderFlow = repository.findById(productOrderId).asFlow();
-
-        if (productOrderFlow.count() < 1) {
-            return;
-        }
-
-        var productOrder = productOrderFlow.toList().first();
-
-        productOrder.partOrders = productOrder.partOrders.plus(listOf(partOrder));
-
-        repository.save(productOrder);
-    }
 }
---
+++ factory_service/src/main/kotlin/trinitityproject/factory/model/Report.kt
@@ -0,0 +1,9 @@
+package trinitityproject.factory.model
+
+import java.util.*
+
+data class Report(
+    var orderCount: Number,
+    val finishedGoodsCount: Number,
+    val costs: Number
+)
