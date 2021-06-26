a309f443134d2c971139b44c9e4b59bcb9a458de
Björn Franke <bjoern.franke@mni.thm.de>
2021-06-26 15:32:05 +0200

    Add condition rpc communication

--- headquarter_service/src/main/kotlin/com/microservices/headquarterservice/Receiver.kt
+++ headquarter_service/src/main/kotlin/com/microservices/headquarterservice/Receiver.kt
@@ -1,14 +1,34 @@
 package com.microservices.headquarterservice
 
+import com.microservices.headquarterservice.model.Condition
+import com.microservices.headquarterservice.model.ConditionRequest
 import org.springframework.stereotype.Component;
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.amqp.rabbit.annotation.Queue
+import org.springframework.amqp.rabbit.annotation.RabbitListener
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.messaging.handler.annotation.Payload
+import java.math.BigDecimal
+import java.time.Instant
+import java.util.*
+
 @Component
 class Receiver {
-  companion object {
-    val logger = LoggerFactory.getLogger(Receiver::class.java)
-  }
-  fun receiveMessage(message: String) {
-    logger.warn("Received <" + message + ">");
-  }
+    companion object {
+        val logger = LoggerFactory.getLogger(Receiver::class.java)
+    }
+
+    @RabbitListener(queuesToDeclare = [Queue(name = "\${microservice.rabbitmq.queue}", durable = "true")])
+    fun receiveOrder(@Payload request: ConditionRequest):Condition {
+        logger.warn("Received <$request>");
+         return Condition(
+            conditions_id = UUID.randomUUID(),
+            supplier_id = UUID.randomUUID(),
+            part_id = UUID.randomUUID(),
+            price = BigDecimal(400),
+            currency = "EURO",
+            negotiation_timestamp = Instant.now()
+        )
+    }
 }
