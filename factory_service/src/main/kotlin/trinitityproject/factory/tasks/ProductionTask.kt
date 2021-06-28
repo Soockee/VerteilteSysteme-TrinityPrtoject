e4af38ea02e3ee68966d83737e51fceb570bb572
Björn Franke <bjoern.franke@mni.thm.de>
2021-06-29 01:31:54 +0200

    Update production-task

--- factory_service/src/main/kotlin/trinitityproject/factory/tasks/ProductionTask.kt
+++ factory_service/src/main/kotlin/trinitityproject/factory/tasks/ProductionTask.kt
@@ -1,16 +1,33 @@
 package trinitityproject.factory.tasks
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import kotlinx.coroutines.flow.retry
+import kotlinx.coroutines.flow.retryWhen
 import kotlinx.coroutines.runBlocking
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.http.MediaType
+import org.springframework.http.codec.ClientCodecConfigurer
+import org.springframework.http.codec.json.Jackson2JsonDecoder
+import org.springframework.http.codec.json.Jackson2JsonEncoder
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.ExchangeStrategies
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToFlow
+import reactor.kotlin.core.publisher.toMono
+import reactor.util.retry.Retry
 import trinitityproject.factory.model.PartOrder
 import trinitityproject.factory.model.Position
 import trinitityproject.factory.model.Status
+import trinitityproject.factory.model.condition.Condition
+import trinitityproject.factory.model.supplier.SupplierOrders
+import trinitityproject.factory.model.supplier.SupplierRequest
+import trinitityproject.factory.model.supplier.SupplierResponse
 import trinitityproject.factory.service.ConditionService
 import trinitityproject.factory.service.PartOrderService
 import trinitityproject.factory.service.ProductOrderService
+import java.time.Duration
 import java.util.*
 
 
@@ -22,7 +39,22 @@ class ProductionTask(
 ) {
     private val log: Logger = LoggerFactory.getLogger(ProductionTask::class.java)
 
-    @Scheduled(fixedRate = 4000)
+    private val jacksonStrategies = ExchangeStrategies
+        .builder()
+        .codecs { clientDefaultCodecsConfigurer: ClientCodecConfigurer ->
+            clientDefaultCodecsConfigurer.defaultCodecs()
+                .jackson2JsonEncoder(Jackson2JsonEncoder(ObjectMapper(), MediaType.APPLICATION_JSON))
+            clientDefaultCodecsConfigurer.defaultCodecs()
+                .jackson2JsonDecoder(Jackson2JsonDecoder(ObjectMapper(), MediaType.APPLICATION_JSON))
+        }.build()
+
+    private val client = WebClient
+        .builder()
+        .baseUrl("http://localhost:8080")
+        .exchangeStrategies(jacksonStrategies)
+        .build()
+
+    @Scheduled(fixedRate = 10000)
     fun scheduleTaskWithFixedRate() {
         runBlocking {
             try {
@@ -49,8 +81,31 @@ class ProductionTask(
                                 }
                             )
                         }
-
+                        .onEach { partOrder ->
+                            val supplierRequest = SupplierRequest(supplierOrders = partOrder.positions
+                                .map {
+                                    SupplierOrders(
+                                        part_id = it.partSupplierId,
+                                        count = it.count
+                                    )
+                                }
+                            )
+                            log.warn(supplierRequest.toString())
+                            val res = client
+                                .post()
+                                .uri("/supplier")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .bodyValue(supplierRequest)
+                                .retrieve()
+                                .bodyToMono(SupplierResponse::class.java)
+                                .retryWhen(Retry.backoff(3, Duration.ofSeconds(2)))
+                                .block()!!
+                            partOrder.orderId = res.supplierId
+                            log.warn("orderID: " + res.supplierId.toString())
+                            partOrder
+                        }
                     log.info("Created: $ll")
+
 
                     productOrder = productOrderService.getOrder(productOrder.productOrderId)
                 }
