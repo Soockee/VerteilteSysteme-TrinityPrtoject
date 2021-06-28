12bf549970cdccda1489e815f88edb1b024e5a3f
Soockee <gamingsockee@gmail.com>
2021-06-28 18:30:24 +0200

    order response

---
+++ headquarter_service/src/main/kotlin/com/microservices/headquarterservice/model/headquarter/order/OrderProductResponse.kt
@@ -0,0 +1,14 @@
+@file:UseSerializers(UUIDSerializer::class)
+package com.microservices.headquarterservice.model.headquarter.order
+
+import com.microservices.headquarterservice.serializer.UUIDSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.UseSerializers
+import org.springframework.data.annotation.Id
+import java.util.*
+
+@Serializable
+class OrderResponse (
+    @Id var customer_id: UUID,
+    var products: List<OrderProductResponse>
+)
