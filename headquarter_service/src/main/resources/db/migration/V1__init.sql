040d10f94dd2b65893570cd1f4a2458bb04698a0
Lars Hieronymi <lars.hieronymi@mni.thm.de>
2021-06-29 00:01:46 +0200

    Add SQL init statements

--- headquarter_service/src/main/resources/db/migration/V1__init.sql
+++ headquarter_service/src/main/resources/db/migration/V1__init.sql
@@ -1,195 +1,216 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE
+EXTENSION IF NOT EXISTS "uuid-ossp";
 
--- CREATE TABLE headquarter (
---     id UUID NOT NULL DEFAULT uuid_generate_v1 (),
---     name character varying(255) NOT NULL,
---     description TEXT,
+CREATE TABLE condition
+(
+    conditions_id         UUID                   NOT NULL DEFAULT uuid_generate_v1(),
+    supplier_id           UUID                   NOT NULL,
+    part_id               UUID                   NOT NULL,
+    price                 NUMERIC(9, 4)          NOT NULL,
+    currency              character varying(255) NOT NULL,
+    negotiation_timestamp TIMESTAMP                       DEFAULT now(),
 
---     PRIMARY KEY(id)
--- );
-
-CREATE TABLE condition (
-    conditions_id UUID NOT NULL DEFAULT uuid_generate_v1 (),
-    supplier_id UUID NOT NULL,
-    part_id UUID NOT NULL,
-    price NUMERIC(9,4) NOT NULL,
-    currency character varying(255) NOT NULL,
-    negotiation_timestamp TIMESTAMP DEFAULT now(),
-
-    PRIMARY KEY(conditions_id)
+    PRIMARY KEY (conditions_id)
 );
 
-CREATE TABLE product (
-    product_id UUID NOT NULL DEFAULT uuid_generate_v1 (),
-    name character varying(255) NOT NULL,
-    production_time INTEGER NOT NULL,
+CREATE TABLE product
+(
+    product_id      UUID                   NOT NULL DEFAULT uuid_generate_v1(),
+    name            character varying(255) NOT NULL,
+    production_time INTEGER                NOT NULL,
 
-    PRIMARY KEY(product_id)
+    PRIMARY KEY (product_id)
 );
 
-CREATE TABLE part (
-    part_id UUID NOT NULL DEFAULT uuid_generate_v1 (),
-    name character varying(255) NOT NULL,
-    delievery_time INTEGER NOT NULL,
-    PRIMARY KEY(part_id)
+CREATE TABLE part
+(
+    part_id        UUID                   NOT NULL DEFAULT uuid_generate_v1(),
+    name           character varying(255) NOT NULL,
+    delievery_time INTEGER                NOT NULL,
+    PRIMARY KEY (part_id)
 );
 
-CREATE TABLE product_part (
-    product_part_id UUID NOT NULL DEFAULT uuid_generate_v1 (),
-    product_id UUID NOT NULL,
-    part_id UUID NOT NULL,
-    count INTEGER NOT NULL,
+CREATE TABLE product_part
+(
+    product_part_id UUID    NOT NULL DEFAULT uuid_generate_v1(),
+    product_id      UUID    NOT NULL,
+    part_id         UUID    NOT NULL,
+    count           INTEGER NOT NULL,
 
-    PRIMARY KEY(product_part_id)
+    PRIMARY KEY (product_part_id)
 );
 
-CREATE TABLE supplier (
-    supplier_id UUID NOT NULL DEFAULT uuid_generate_v1 (),
-    name character varying(255) NOT NULL,
-    PRIMARY KEY(supplier_id)
+CREATE TABLE supplier
+(
+    supplier_id UUID                   NOT NULL DEFAULT uuid_generate_v1(),
+    name        character varying(255) NOT NULL,
+    PRIMARY KEY (supplier_id)
 );
 
-CREATE TABLE orders (
-    order_id UUID NOT NULL DEFAULT uuid_generate_v1 (),
-    customer_id UUID NOT NULL DEFAULT uuid_generate_v1 (), -- TODO: Need Customer Managment
-    begin_order TIMESTAMP NOT NULL DEFAULT now(),
-    status character varying(255) NOT NULL,
+CREATE TABLE orders
+(
+    order_id    UUID                   NOT NULL DEFAULT uuid_generate_v1(),
+    customer_id UUID                   NOT NULL DEFAULT uuid_generate_v1(), -- TODO: Need Customer Managment
+    begin_order TIMESTAMP              NOT NULL DEFAULT now(),
+    status      character varying(255) NOT NULL,
 
     PRIMARY KEY (order_id)
 );
 
-CREATE TABLE order_product (
-    order_product_id UUID NOT NULL DEFAULT uuid_generate_v1 (),
-    order_id UUID NOT NULL DEFAULT uuid_generate_v1 (),
-    product_id UUID NOT NULL DEFAULT uuid_generate_v1 (),
-    count INTEGER NOT NULL,
+CREATE TABLE order_product
+(
+    order_product_id UUID    NOT NULL DEFAULT uuid_generate_v1(),
+    order_id         UUID    NOT NULL DEFAULT uuid_generate_v1(),
+    product_id       UUID    NOT NULL DEFAULT uuid_generate_v1(),
+    count            INTEGER NOT NULL,
 
-    PRIMARY KEY(order_product_id)
+    PRIMARY KEY (order_product_id)
 );
 
-CREATE TABLE support_ticket (
-    support_ticket_id UUID NOT NULL DEFAULT uuid_generate_v1 (),
-    customer_id UUID NOT NULL,
-    status character varying(255) NOT NULL,
-    create_time TIMESTAMP NOT NULL DEFAULT now(),
-
-    PRIMARY KEY(support_ticket_id)
+CREATE TABLE supplier_order_part
+(
+    supplier_order_part_id UUID    NOT NULL DEFAULT uuid_generate_v1(),
+    supplier_order_id      UUID    NOT NULL,
+    part_id                UUID    NOT NULL,
+    count                  INTEGER NOT NULL,
+    PRIMARY KEY (supplier_order_part_id)
 );
 
-CREATE TABLE support_ticket_text (
-    support_ticket_text_id UUID NOT NULL DEFAULT uuid_generate_v1 (),
-    support_ticket_id UUID NOT NULL,
-    text character varying(255) NOT NULL,
-    change_time TIMESTAMP NOT NULL DEFAULT now(),
-
-    PRIMARY KEY (support_ticket_text_id)
-);
-
-
-CREATE TABLE supplier_order_part (
-     supplier_order_part_id UUID NOT NULL DEFAULT uuid_generate_v1 (),
-     supplier_order_id UUID NOT NULL ,
-     part_id UUID NOT NULL,
-     count INTEGER NOT NULL,
-     PRIMARY KEY (supplier_order_part_id)
-);
-
-CREATE TABLE supplier_order (
-    order_id UUID NOT NULL DEFAULT uuid_generate_v1 (),
-    supplier_id UUID NOT NULL DEFAULT uuid_generate_v1 (),
-    begin_order  TIMESTAMP NOT NULL DEFAULT now(),
-    status character varying(255) NOT NULL,
-    PRIMARY KEY(order_id)
+CREATE TABLE supplier_order
+(
+    order_id    UUID                   NOT NULL DEFAULT uuid_generate_v1(),
+    supplier_id UUID                   NOT NULL DEFAULT uuid_generate_v1(),
+    begin_order TIMESTAMP              NOT NULL DEFAULT now(),
+    status      character varying(255) NOT NULL,
+    PRIMARY KEY (order_id)
 );
 
 
-
-INSERT INTO supplier (supplier_id,name)
-VALUES('8a34a8fe-d419-11eb-b8bc-0242ac130003', 'electroStuff.com');
-INSERT INTO supplier (supplier_id,name)
-VALUES('8d017b98-d419-11eb-b8bc-0242ac130003', 'CoolMechanics.com');
+-- Add suppliers
+INSERT INTO supplier (supplier_id, name)
+VALUES ('8a34a8fe-d419-11eb-b8bc-0242ac130003', 'electroStuff.com');
+INSERT INTO supplier (supplier_id, name)
+VALUES ('8d017b98-d419-11eb-b8bc-0242ac130003', 'CoolMechanics.com');
 
 -- Add Parts
-INSERT INTO part (part_id,name,delievery_time)
-VALUES('fa67169c-d4e2-11eb-b8bc-0242ac130003','Schraube 3x18',10);
-INSERT INTO part (part_id,name,delievery_time)
-VALUES('fa67191c-d4e2-11eb-b8bc-0242ac130003','Schraube 3x16',5);
-INSERT INTO part (part_id,name,delievery_time)
-VALUES('fa671a02-d4e2-11eb-b8bc-0242ac130003','Tür 100x60',40);
-INSERT INTO part (part_id,name,delievery_time)
-VALUES('fa671aca-d4e2-11eb-b8bc-0242ac130003','Korpus 100x60x55',50);
-INSERT INTO part (part_id,name,delievery_time)
-VALUES('7f58bfe8-d418-11eb-b8bc-0242ac130003','Kühlmotor',100);
+INSERT INTO part (part_id, name, delievery_time)
+VALUES ('fa67191c-d4e2-11eb-b8bc-0242ac130003', 'Schraube 3x16', 10);
+INSERT INTO part (part_id, name, delievery_time)
+VALUES ('fa67169c-d4e2-11eb-b8bc-0242ac130003', 'Schraube 3x18', 15);
+INSERT INTO part (part_id, name, delievery_time)
+VALUES ('10b695e8-d66a-11eb-b8bc-0242ac130003', 'Schraube 4x20', 20);
+INSERT INTO part (part_id, name, delievery_time)
+VALUES ('10b69ff2-d66a-11eb-b8bc-0242ac130003', 'Kühlbox 60cmx60cmx60cm', 100);
+INSERT INTO part (part_id, name, delievery_time)
+VALUES ('10b6a0b0-d66a-11eb-b8bc-0242ac130003', 'Kühlbox Deckel 60cmx60cm', 100);
+INSERT INTO part (part_id, name, delievery_time)
+VALUES ('fa671a02-d4e2-11eb-b8bc-0242ac130003', 'Tür Größe S 100cmx60cm', 100);
+INSERT INTO part (part_id, name, delievery_time)
+VALUES ('fa671aca-d4e2-11eb-b8bc-0242ac130003', 'Korpus Größe S 100cmx60cmx55cm', 100);
+INSERT INTO part (part_id, name, delievery_time)
+VALUES ('7f58bfe8-d418-11eb-b8bc-0242ac130003', 'Kühlmotor klein', 190);
+INSERT INTO part (part_id, name, delievery_time)
+VALUES ('10b6985e-d66a-11eb-b8bc-0242ac130003', 'Tür Größe M 150cmx60cm', 100);
+INSERT INTO part (part_id, name, delievery_time)
+VALUES ('10b69944-d66a-11eb-b8bc-0242ac130003', 'Korpus Größe M 150cmx60cmx55cm', 100);
+INSERT INTO part (part_id, name, delievery_time)
+VALUES ('10b69a0c-d66a-11eb-b8bc-0242ac130003', 'Kühlmotor groß', 200);
+INSERT INTO part (part_id, name, delievery_time)
+VALUES ('10b69aca-d66a-11eb-b8bc-0242ac130003', 'Tür Größe L 200cmx60cm', 100);
+INSERT INTO part (part_id, name, delievery_time)
+VALUES ('10b69d4a-d66a-11eb-b8bc-0242ac130003', 'Korpus Größe L 200cmx60cmx55cm', 100);
+INSERT INTO part (part_id, name, delievery_time)
+VALUES ('10b69e6c-d66a-11eb-b8bc-0242ac130003', 'Korpus Größe XL 200cmx120cmx55cm', 100);
 
 -- Add Product
 INSERT INTO product (product_id, name, production_time)
-VALUES ('7174a632-d4e3-11eb-b8bc-0242ac130003','Kühlschrank Größe S 100x60x60', 100);
+VALUES ('9e9383ca-d855-11eb-b8bc-0242ac130003', 'Kühlbox 60cmx60cmx60cm - Modell: Die Studentenbox', 100);
 INSERT INTO product (product_id, name, production_time)
-VALUES ('82a637fe-d4e3-11eb-b8bc-0242ac130003','Kühlschrank Größe M 150x60x60', 200);
+VALUES ('7174a632-d4e3-11eb-b8bc-0242ac130003',
+        'Kühlschrank Größe S 100cmx60cmx60cm - Modell: Der Kühlschrank für Singles', 100);
 INSERT INTO product (product_id, name, production_time)
-VALUES ('8664853a-d4e3-11eb-b8bc-0242ac130003','Kühlschrank Größe L 200x60x60 Modell: Aus dem Weg Kleinverdiener', 300);
+VALUES ('82a637fe-d4e3-11eb-b8bc-0242ac130003',
+        'Kühlschrank Größe M 150cmx60cmx60cm - Modell: Der Kühlschrank für Pärchen', 200);
+INSERT INTO product (product_id, name, production_time)
+VALUES ('8664853a-d4e3-11eb-b8bc-0242ac130003', 'Kühlschrank Größe L 200cmx60cmx60cm - Modell: Für die ganze Familie',
+        300);
+INSERT INTO product (product_id, name, production_time)
+VALUES ('10b69f3e-d66a-11eb-b8bc-0242ac130003',
+        'Kühlschrank Größe XL 200cmx120cmx60cm - Modell: Aus dem Weg Kleinverdiener', 400);
 
 -- Add Product_Part
--- Kühlschrank Größe S 100x60x60
 INSERT INTO product_part (product_id, part_id, count)
-VALUES ('7174a632-d4e3-11eb-b8bc-0242ac130003', 'fa671aca-d4e2-11eb-b8bc-0242ac130003', 1); -- Korpus
+VALUES ('9e9383ca-d855-11eb-b8bc-0242ac130003', 'fa67191c-d4e2-11eb-b8bc-0242ac130003', 19);
 INSERT INTO product_part (product_id, part_id, count)
-VALUES ('7174a632-d4e3-11eb-b8bc-0242ac130003', 'fa671a02-d4e2-11eb-b8bc-0242ac130003', 1); -- Tür
+VALUES ('9e9383ca-d855-11eb-b8bc-0242ac130003', '10b69ff2-d66a-11eb-b8bc-0242ac130003', 1);
 INSERT INTO product_part (product_id, part_id, count)
-VALUES ('7174a632-d4e3-11eb-b8bc-0242ac130003', '7f58bfe8-d418-11eb-b8bc-0242ac130003', 1); -- Kühlmotor
-INSERT INTO product_part (product_id, part_id, count)
-VALUES ('7174a632-d4e3-11eb-b8bc-0242ac130003', 'fa67191c-d4e2-11eb-b8bc-0242ac130003', 10); -- Schraube 3x16
+VALUES ('9e9383ca-d855-11eb-b8bc-0242ac130003', '10b6a0b0-d66a-11eb-b8bc-0242ac130003', 1);
 
+INSERT INTO product_part (product_id, part_id, count)
+VALUES ('7174a632-d4e3-11eb-b8bc-0242ac130003', 'fa67191c-d4e2-11eb-b8bc-0242ac130003', 17);
+INSERT INTO product_part (product_id, part_id, count)
+VALUES ('7174a632-d4e3-11eb-b8bc-0242ac130003', 'fa671a02-d4e2-11eb-b8bc-0242ac130003', 1);
+INSERT INTO product_part (product_id, part_id, count)
+VALUES ('7174a632-d4e3-11eb-b8bc-0242ac130003', 'fa671aca-d4e2-11eb-b8bc-0242ac130003', 1);
+INSERT INTO product_part (product_id, part_id, count)
+VALUES ('7174a632-d4e3-11eb-b8bc-0242ac130003', '7f58bfe8-d418-11eb-b8bc-0242ac130003', 1);
 
--- Add Product_Part
--- Kühlschrank Größe M 150x60x60
 INSERT INTO product_part (product_id, part_id, count)
-VALUES ('82a637fe-d4e3-11eb-b8bc-0242ac130003', 'fa671aca-d4e2-11eb-b8bc-0242ac130003', 2); -- Korpus
+VALUES ('82a637fe-d4e3-11eb-b8bc-0242ac130003', 'fa67191c-d4e2-11eb-b8bc-0242ac130003', 19);
 INSERT INTO product_part (product_id, part_id, count)
-VALUES ('82a637fe-d4e3-11eb-b8bc-0242ac130003', 'fa671a02-d4e2-11eb-b8bc-0242ac130003', 2); -- Tür
+VALUES ('82a637fe-d4e3-11eb-b8bc-0242ac130003', '10b6985e-d66a-11eb-b8bc-0242ac130003', 1);
 INSERT INTO product_part (product_id, part_id, count)
-VALUES ('82a637fe-d4e3-11eb-b8bc-0242ac130003', '7f58bfe8-d418-11eb-b8bc-0242ac130003', 2); -- Kühlmotor
+VALUES ('82a637fe-d4e3-11eb-b8bc-0242ac130003', '10b69944-d66a-11eb-b8bc-0242ac130003', 1);
 INSERT INTO product_part (product_id, part_id, count)
-VALUES ('82a637fe-d4e3-11eb-b8bc-0242ac130003', 'fa67191c-d4e2-11eb-b8bc-0242ac130003', 20); -- Schraube 3x16
+VALUES ('82a637fe-d4e3-11eb-b8bc-0242ac130003', '7f58bfe8-d418-11eb-b8bc-0242ac130003', 1);
 
--- Add Product_Part
--- Kühlschrank Größe L 200x60x60 Modell: Aus dem Weg Kleinverdiener
 INSERT INTO product_part (product_id, part_id, count)
-VALUES ('8664853a-d4e3-11eb-b8bc-0242ac130003', 'fa671aca-d4e2-11eb-b8bc-0242ac130003', 30); -- Korpus
+VALUES ('8664853a-d4e3-11eb-b8bc-0242ac130003', 'fa67169c-d4e2-11eb-b8bc-0242ac130003', 23);
 INSERT INTO product_part (product_id, part_id, count)
-VALUES ('8664853a-d4e3-11eb-b8bc-0242ac130003', 'fa671a02-d4e2-11eb-b8bc-0242ac130003', 30); -- Tür
+VALUES ('8664853a-d4e3-11eb-b8bc-0242ac130003', '10b69aca-d66a-11eb-b8bc-0242ac130003', 1);
 INSERT INTO product_part (product_id, part_id, count)
-VALUES ('8664853a-d4e3-11eb-b8bc-0242ac130003', '7f58bfe8-d418-11eb-b8bc-0242ac130003', 30); -- Kühlmotor
+VALUES ('8664853a-d4e3-11eb-b8bc-0242ac130003', '10b69d4a-d66a-11eb-b8bc-0242ac130003', 1);
 INSERT INTO product_part (product_id, part_id, count)
-VALUES ('8664853a-d4e3-11eb-b8bc-0242ac130003', 'fa67191c-d4e2-11eb-b8bc-0242ac130003', 300); -- Schraube 3x16
+VALUES ('8664853a-d4e3-11eb-b8bc-0242ac130003', '10b69a0c-d66a-11eb-b8bc-0242ac130003', 1);
 
-
+INSERT INTO product_part (product_id, part_id, count)
+VALUES ('10b69f3e-d66a-11eb-b8bc-0242ac130003', '10b695e8-d66a-11eb-b8bc-0242ac130003', 38);
+INSERT INTO product_part (product_id, part_id, count)
+VALUES ('10b69f3e-d66a-11eb-b8bc-0242ac130003', 'fa67169c-d4e2-11eb-b8bc-0242ac130003', 9);
+INSERT INTO product_part (product_id, part_id, count)
+VALUES ('10b69f3e-d66a-11eb-b8bc-0242ac130003', '10b69e6c-d66a-11eb-b8bc-0242ac130003', 1);
+INSERT INTO product_part (product_id, part_id, count)
+VALUES ('10b69f3e-d66a-11eb-b8bc-0242ac130003', '10b69aca-d66a-11eb-b8bc-0242ac130003', 2);
+INSERT INTO product_part (product_id, part_id, count)
+VALUES ('10b69f3e-d66a-11eb-b8bc-0242ac130003', '10b69a0c-d66a-11eb-b8bc-0242ac130003', 1);
 
 -- Add Condition
+INSERT INTO condition (supplier_id, part_id, price, currency)
+VALUES ('8a34a8fe-d419-11eb-b8bc-0242ac130003', 'fa67191c-d4e2-11eb-b8bc-0242ac130003', 0.10, 'EUR');
+INSERT INTO condition (supplier_id, part_id, price, currency)
+VALUES ('8a34a8fe-d419-11eb-b8bc-0242ac130003', 'fa67169c-d4e2-11eb-b8bc-0242ac130003', 0.12, 'EUR');
+INSERT INTO condition (supplier_id, part_id, price, currency)
+VALUES ('8a34a8fe-d419-11eb-b8bc-0242ac130003', '10b695e8-d66a-11eb-b8bc-0242ac130003', 0.18, 'EUR');
+INSERT INTO condition (supplier_id, part_id, price, currency)
+VALUES ('8a34a8fe-d419-11eb-b8bc-0242ac130003', '7f58bfe8-d418-11eb-b8bc-0242ac130003', 50.99, 'EUR');
+INSERT INTO condition (supplier_id, part_id, price, currency)
+VALUES ('8a34a8fe-d419-11eb-b8bc-0242ac130003', '10b69a0c-d66a-11eb-b8bc-0242ac130003', 79.99, 'EUR');
 
-INSERT INTO condition (supplier_id,part_id,price,currency)
-VALUES('8a34a8fe-d419-11eb-b8bc-0242ac130003','7f58be62-d418-11eb-b8bc-0242ac130003', 0.001 ,'Euro');
-INSERT INTO condition (supplier_id,part_id,price,currency)
-VALUES('8a34a8fe-d419-11eb-b8bc-0242ac130003','7f58bc28-d418-11eb-b8bc-0242ac130003', 110.5,'Euro');
-
-INSERT INTO condition (supplier_id,part_id,price,currency)
-VALUES('8d017b98-d419-11eb-b8bc-0242ac130003','7f58bfe8-d418-11eb-b8bc-0242ac130003', 1.55,'USD');
-
-INSERT INTO condition (supplier_id,part_id,price,currency)
-VALUES('8a34a8fe-d419-11eb-b8bc-0242ac130003','7f58bfe8-d418-11eb-b8bc-0242ac130003', 1.70,'USD');
-INSERT INTO condition (supplier_id,part_id,price,currency)
-VALUES('8d017b98-d419-11eb-b8bc-0242ac130003','93338fb6-d418-11eb-b8bc-0242ac130003', 20.0,'USD');
-INSERT INTO condition (supplier_id,part_id,price,currency)
-VALUES('8a34a8fe-d419-11eb-b8bc-0242ac130003','fa671a02-d4e2-11eb-b8bc-0242ac130003', 8888.8,'Euro');
-
-
-
-
--- INSERT INTO condition (supplier_id,part_id,price,currency)
--- VALUES('8a34a8fe-d419-11eb-b8bc-0242ac130003','fa671aca-d4e2-11eb-b8bc-0242ac130003', 0.001 ,'Euro');
-INSERT INTO condition (supplier_id,part_id,price,currency)
-VALUES('8a34a8fe-d419-11eb-b8bc-0242ac130003','fa67191c-d4e2-11eb-b8bc-0242ac130003', 6666.6,'USD');
-INSERT INTO condition (supplier_id,part_id,price,currency)
-VALUES('8d017b98-d419-11eb-b8bc-0242ac130003','93338fb6-d418-11eb-b8bc-0242ac130003', 5555.5,'USD');
-
-
+INSERT INTO condition (supplier_id, part_id, price, currency)
+VALUES ('8d017b98-d419-11eb-b8bc-0242ac130003', '10b69ff2-d66a-11eb-b8bc-0242ac130003', 19.99, 'EUR');
+INSERT INTO condition (supplier_id, part_id, price, currency)
+VALUES ('8d017b98-d419-11eb-b8bc-0242ac130003', '10b6a0b0-d66a-11eb-b8bc-0242ac130003', 5.99, 'EUR');
+INSERT INTO condition (supplier_id, part_id, price, currency)
+VALUES ('8d017b98-d419-11eb-b8bc-0242ac130003', 'fa671a02-d4e2-11eb-b8bc-0242ac130003', 17.99, 'EUR');
+INSERT INTO condition (supplier_id, part_id, price, currency)
+VALUES ('8d017b98-d419-11eb-b8bc-0242ac130003', 'fa671aca-d4e2-11eb-b8bc-0242ac130003', 59.99, 'EUR');
+INSERT INTO condition (supplier_id, part_id, price, currency)
+VALUES ('8d017b98-d419-11eb-b8bc-0242ac130003', '10b6985e-d66a-11eb-b8bc-0242ac130003', 29.99, 'EUR');
+INSERT INTO condition (supplier_id, part_id, price, currency)
+VALUES ('8d017b98-d419-11eb-b8bc-0242ac130003', '10b69944-d66a-11eb-b8bc-0242ac130003', 99.99, 'EUR');
+INSERT INTO condition (supplier_id, part_id, price, currency)
+VALUES ('8d017b98-d419-11eb-b8bc-0242ac130003', '10b69aca-d66a-11eb-b8bc-0242ac130003', 59.99, 'EUR');
+INSERT INTO condition (supplier_id, part_id, price, currency)
+VALUES ('8d017b98-d419-11eb-b8bc-0242ac130003', '10b69d4a-d66a-11eb-b8bc-0242ac130003', 149.99, 'EUR');
+INSERT INTO condition (supplier_id, part_id, price, currency)
+VALUES ('8d017b98-d419-11eb-b8bc-0242ac130003', '10b69e6c-d66a-11eb-b8bc-0242ac130003', 329.99, 'EUR');
