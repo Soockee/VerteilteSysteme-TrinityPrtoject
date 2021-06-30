d6b130696f4aca0760f545223e88510614bacef8
Björn Franke <bjoern.franke@mni.thm.de>
2021-06-30 03:48:02 +0200

    Fix prop-name

--- factory_service/src/main/kotlin/trinitityproject/factory/config/RabbitMqConfig.kt
+++ factory_service/src/main/kotlin/trinitityproject/factory/config/RabbitMqConfig.kt
@@ -3,16 +3,25 @@ package trinitityproject.factory.config
 import org.springframework.amqp.core.Queue
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
 import org.springframework.amqp.support.converter.MessageConverter
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 
 @Configuration
-class RabbitMqConfig {
+class RabbitMqConfig(
+    @Value("\${factory.conditionQueueName}") private val conditionQueue: String,
+    @Value("\${factory.orderQueueName}") private val orderQueue: String
+) {
 
     @Bean
-    fun queue(): Queue {
-        return Queue("conditionRequests")
+    fun conditionQueue(): Queue {
+        return Queue(conditionQueue)
+    }
+
+    @Bean
+    fun orderQueue(): Queue {
+        return Queue(orderQueue)
     }
 
     // The jackson is set for the rabbitmq RPC-call
